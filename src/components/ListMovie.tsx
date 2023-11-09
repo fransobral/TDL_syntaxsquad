ab6058fdb321e923e763db995ec2b05c269f1a64
Nelson Avila <nbrian.avila96@gmail.com>
2023-11-09 14:48:48 -0300

    Se comienza con front | resta asociar con api

---
+++ src/components/ListMovie.tsx
@@ -0,0 +1,12 @@
+type Props = {
+    listMovies: string[]
+    deleteMovies: (index:number) => void
+}
+
+const ListMovie = (props: Props) => {
+  return (
+    <div>ListMovie</div>
+  )
+}
+
+export default ListMovie
