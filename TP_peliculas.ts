797598800d0d889abec9f8a4c17bde801a35dd1f
Jacqueline Oura <jacquelineoura@gmail.com>
2023-11-22 22:10:45 -0300

    en proceso llamada por nombre de pelicula
    en proceso llamada por nombre de pelicula

--- TP_peliculas.ts
+++ TP_peliculas.ts
@@ -72,6 +72,40 @@ class MovieDB {
         throw new Error('No se encontraron películas para el género especificado.');
         }
     }
+
+    async searchMovieByFName(movieName) {
+      try {
+        // Paso 1: Buscar la película por nombre
+        const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${encodeURI(movieName)}`;
+        const searchResponse = await fetch(searchUrl);
+    
+        if (!searchResponse.ok) {
+          throw new Error(`Error en la búsqueda de películas. Código de estado: ${searchResponse.status}`);
+        }
+    
+        const searchData = await searchResponse.json();
+    
+        // Paso 2: Obtener información detallada de la primera película encontrada
+        if (searchData.results && searchData.results.length > 0) {
+          const firstMovie = searchData.results[0];
+          const movieId = firstMovie.id;
+    
+          const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.apiKey}`;
+          const movieResponse = await fetch(movieUrl);
+    
+          if (!movieResponse.ok) {
+            throw new Error(`Error when retrieving movie information. Status code: ${movieResponse.status}`);
+          }
+    
+          const movieData = await movieResponse.json();
+          return movieData;
+        } else {
+          throw new Error('No results were found for the movie.');
+        }
+      } catch (error) {
+        throw new Error(`Error in the movie search.: ${error.message}`);
+      }
+  }
 }
 
 const movieDB = new MovieDB('08c6fb59f7c71d29805136fe34281282');
@@ -109,5 +143,37 @@ movieDB.searchMoviesByActor(actor)
   .catch(error => {
     console.error(`Error: ${error.message}`);
   });
+
+
+
+ // const movieDB = new MovieDB('08c6fb59f7c71d29805136fe34281282');
+//  adult:false
+//  backdrop_path:'/r0kZNywAeN6Ar75rxDqLlTP5RiJ.jpg'
+//  genre_ids:(3) [80, 53, 18]
+//  id:475557
+//  original_language:'en'
+//  original_title:'Joker'
+//  overview:
+//  'During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.'
+//  popularity:92.884
+//  poster_path:'/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg'
+//  release_date:'2019-10-01'
+//  title:'Joker'
+//  video:false
+//  vote_average:8.165
+//  vote_count:23667
+const filmName = "Joker";
+  // Buscar peli por nombre
+movieDB.searchMovieByFName(filmName)
+.then(movies => {
+  console.log(``);
+  console.log(`Pelicula: ${filmName}`);
+     
+  console.log(`${movies[0] }`);
+  }  
+)
+.catch(error => {
+  console.error(`Error: ${error.message}`);
+});
 
 
